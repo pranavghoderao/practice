bddc80c58226d2a6c53df5f8c6b8928ae38dd2ee
ghoderao <pranavghoderao@gmail.com>
2019-12-03 23:22:11 -0800

    first commit

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a=11;
+int b=22;
+printf("addition is %d",a+b);
+
+return 0;
+}
---
+++ sub.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a=22;
+int b=11;
+printf("addition is %d",a-b);
+
+return 0;
+}
